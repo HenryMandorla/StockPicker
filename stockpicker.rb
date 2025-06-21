decfd53cde08c7163e479b41d201a59ce6a620ed
Henry Mandorla <henry.mandorla@gmail.com>
2025-06-21 19:41:34 +1000

    stock picker function

---
+++ stockpicker.rb
@@ -0,0 +1,20 @@
+def stock_picker(prices)
+  buy = nil
+  sell = nil
+  best_profit = 0
+  prices.each_with_index do |buy_price, buy_day|
+    prices.each_with_index do |sell_price, sell_day|
+      if sell_day > buy_day
+        profit = sell_price - buy_price
+        if profit > best_profit
+          best_profit = profit
+          buy = buy_day
+          sell = sell_day
+        end
+      end
+    end
+  end
+  return [buy, sell, best_profit]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
